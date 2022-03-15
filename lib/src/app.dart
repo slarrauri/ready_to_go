c69a36524a88ff617f820d65c59781b5e88c2171
slarrauri <slarrauri@gmail.com>
2022-03-15 15:17:50 -0300

    fix: isInitialRoute

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -6,7 +6,7 @@ import 'package:ready_to_go/src/app/ui/pages/home_page.dart';
 import 'package:ready_to_go/src/config/flavor_banner.dart';
 
 class MyApp extends StatelessWidget {
-
+  bool isInitialRoute = true;
   @override
   Widget build(BuildContext context) {
     return BlocProvider<ApplicationBloc>(
@@ -19,25 +19,21 @@ class MyApp extends StatelessWidget {
   }
 
   Route _routes(RouteSettings settings) {
-    if (settings.isInitialRoute)
-      return MaterialPageRoute(
-          builder: (context) {
-            final homePage = HomePage();
+    return MaterialPageRoute(builder: (context) {
+      final homePage = HomePage();
 
-            final applicationBloc = BlocProvider.of<ApplicationBloc>(context);
-            applicationBloc.listenConnectivity(homePage);
+      final applicationBloc = BlocProvider.of<ApplicationBloc>(context);
+      applicationBloc.listenConnectivity(homePage);
 
-            final homebloc = HomeBloc();
-            homebloc.fetchData(homePage);
+      final homebloc = HomeBloc();
+      homebloc.fetchData(homePage);
 
-            return BlocProvider<HomeBloc>(
-              bloc: homebloc,
-              child: FlavorBanner(
-                child: homePage,
-              ),
-            );
-          }
+      return BlocProvider<HomeBloc>(
+        bloc: homebloc,
+        child: FlavorBanner(
+          child: homePage,
+        ),
       );
+    });
   }
-
 }
